2a4fa7206759e0d6cc7df33491943b488ac4ea38
tnhnkzc <tunahankuzucuu@gmail.com>
2022-05-04 12:01:35 +0200

    Heroku Created

--- app.js
+++ app.js
@@ -120,6 +120,6 @@ app.post("/weather", function (req, res) {
 });
 
 // Server
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("The server is running on port 3000");
 });
